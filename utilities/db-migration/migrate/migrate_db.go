852a10568384d38cf7bd0f0a3384e3140af6b549
anandf <anandfrancis.joseph@gmail.com>
2023-02-21 23:59:48 +0530

    Added new Dockerfile for db-migration
    Signed-off-by: anandf <anandfrancis.joseph@gmail.com>

--- utilities/db-migration/migrate/migrate_db.go
+++ utilities/db-migration/migrate/migrate_db.go
@@ -2,16 +2,19 @@ package migrate
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	migrate "github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	db "github.com/redhat-appstudio/managed-gitops/backend-shared/db"
+
+	"github.com/go-pg/pg/extra/pgdebug"
+	"github.com/go-pg/pg/v10"
 )
 
 func Migrate(opType string, migrationPath string) error {
-	addr, password := db.GetAddrAndPassword()
+	addr, password := GetAddrAndPassword()
 	port := 5432
 
 	// Base64 strings can contain '/' characters, which mess up URL parsing.
@@ -34,7 +37,7 @@ func Migrate(opType string, migrationPath string) error {
 		return nil
 
 	} else if opType == "drop_smtable" {
-		dbq, err := db.ConnectToDatabaseWithPort(true, "postgres", port)
+		dbq, err := ConnectToDatabaseWithPort(true, "postgres", port)
 		if err != nil {
 			return fmt.Errorf("unable to connect to DB: %v", err)
 		} else {
@@ -65,4 +68,57 @@ func Migrate(opType string, migrationPath string) error {
 		return fmt.Errorf("invalid argument passed")
 	}
 
+}
+
+func GetAddrAndPassword() (string, string) {
+	addr := "localhost"
+	if isEnvExist("DB_ADDR") {
+		addr = os.Getenv("DB_ADDR")
+	}
+
+	password := "gitops"
+	if isEnvExist("DB_PASS") {
+		password = os.Getenv("DB_PASS")
+	}
+	return addr, password
+}
+
+// connectToDatabaseWithPort connects to Postgres with a defined port
+func ConnectToDatabaseWithPort(verbose bool, dbName string, port int) (*pg.DB, error) {
+	addr, password := GetAddrAndPassword()
+	opts := &pg.Options{
+		Addr:     fmt.Sprintf("%s:%v", addr, port),
+		User:     "postgres",
+		Password: password,
+		Database: dbName,
+	}
+
+	db := pg.Connect(opts)
+
+	if err := checkConn(db); err != nil {
+		return nil, fmt.Errorf("%v, unable to connect to database: Host:'%s' User:'%s' Pass:'%s' DB:'%s' ", err, opts.Addr, opts.User, opts.Password, opts.Database)
+	}
+
+	if verbose {
+		db.AddQueryHook(pgdebug.DebugHook{
+			// Print all queries.
+			Verbose: true,
+		})
+	}
+
+	return db, nil
+}
+
+func isEnvExist(key string) bool {
+	if _, ok := os.LookupEnv(key); ok {
+		return true
+	}
+
+	return false
+}
+
+func checkConn(db *pg.DB) error {
+	var n int
+	_, err := db.QueryOne(pg.Scan(&n), "SELECT 1")
+	return err
 }
